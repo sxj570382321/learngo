4636b0fef59ba7cc346f369bee5d860f46cc25f8
sxj570382321 <570382321@qq.com>
2018-03-22 20:32:25 +0800

    commit changes of interface.go and type_convert.go

--- interface.go
+++ interface.go
@@ -2,44 +2,40 @@ package main
 
 import "fmt"
 
-type Phone interface{
+type Phone interface {
 	call()
 }
 
-type NokiaPhone struct{
-
-
+type NokiaPhone struct {
 }
 
-func (nk NokiaPhone) add() int{
+func (nk NokiaPhone) add() int {
 	fmt.Println("this is add of NokiaPhone")
 	return 0
 }
 
-
-func (nokiaPhone NokiaPhone) call(){
+func (nokiaPhone NokiaPhone) call() {
 	fmt.Println("I am Nokia,I can call you!")
 }
 
-type Iphone struct{
-
+type Iphone struct {
 }
 
-func (iphone Iphone) call(){
+func (iphone Iphone) call() {
 	fmt.Println("I am Iphone,I can call you!")
 }
 
-func main(){
-	
+func main() {
+	var aaaaa
 	var phone Phone
 	phone = new(NokiaPhone)
 	phone.call()
-//	phone.add()
+	//	phone.add()
 
-	var phone1* NokiaPhone = new(NokiaPhone)
+	var phone1 *NokiaPhone = new(NokiaPhone)
 	phone1.add()
-	
-	var phone2* NokiaPhone
+
+	var phone2 *NokiaPhone
 	phone2 = new(NokiaPhone)
 	phone2.add()
 
--- type_convert.go
+++ type_convert.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
 	var sum int = 17
 	var count int = 5
 	var mean float32
+	var a = 65
 
 	mean = float32(sum) / float32(count)
-	fmt.Printf("mean is %f\n",mean)
-
+	fmt.Printf("mean is %f\n", mean)
+	b := string(a)
+	fmt.Println("b is:", b)
 }
